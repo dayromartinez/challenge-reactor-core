8c0e147d58681d8925595eb41f39fc35c4b843c7
Dayro Martínez <djmd96@hotmail.com>
2021-12-13 22:21:47 -0500

    Ruta de consulta general y filtrado por edad OK!

--- src/main/java/com/example/demo/Models/PlayerDb.java
+++ src/main/java/com/example/demo/Models/PlayerDb.java
@@ -3,29 +3,29 @@ package com.example.demo.Models;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collation = "Jugadores")
+@Document(collection = "players")
 public class PlayerDb {
 
     @Id
-    private Integer id;
+    private String id;
     public String name;
     public int age;
     public String icon;
     public String national;
     public int winners;
-    public int games;
+    public int gamers;
     public double ranking;
     public String club;
 
-    public PlayerDb(Integer id, String name, int age, String icon, String national, int winners, int games, String club) {
+    public PlayerDb(String id, String name, int age, String icon, String national, int winners, int gamers, double ranking, String club) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.icon = icon;
         this.national = national;
         this.winners = winners;
-        this.games = games;
-        this.ranking = winners / games;
+        this.gamers = gamers;
+        this.ranking = ranking;
         this.club = club;
     }
 
@@ -49,8 +49,8 @@ public class PlayerDb {
         return winners;
     }
 
-    public int getGames() {
-        return games;
+    public int getGamers() {
+        return gamers;
     }
 
     public double getRanking() {
